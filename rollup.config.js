98a01c2683b9d7bc8b11072f1ac77e1984b9aa3c
Cássio Rodrigues <curriculocassio@hotmail.com>
2023-08-05 14:04:18 -0300

    Fix bugs com dependencias

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,23 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
+import { yourFunction } from "rollup-plugin-your-function";
+
+const terser = () => yourFunction({
+    output: true,
+    name: "terser",
+    fn: async (source, options) => minify(
+        source,
+        {
+            module: (/^esm?$/).test(options.outputOptions.format),
+            toplevel: options.outputOptions.format === "cjs",
+            sourceMap: true
+        }
+    )
+});
 
 export default [
   {
